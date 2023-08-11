f814bfcd05b3d51c077139af6651523cd7a1d775
Sucellus Agroambiental <140858890+SucellusAgroambiental@users.noreply.github.com>
2023-08-11 10:40:52 -0300

    Add files via upload

---
+++ styles/TalhesProdutivos_13_style.js
@@ -0,0 +1,112 @@
+var size = 0;
+var placement = 'point';
+function categories_TalhesProdutivos_13(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'T01':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(87,211,190,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T02':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(106,231,64,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T03':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(208,236,94,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T04':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(218,74,136,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T05':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(132,65,208,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T07':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(33,220,89,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T08':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(226,68,223,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T09':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(210,146,28,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T10':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(40,140,203,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'T11':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(22,35,220,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_TalhesProdutivos_13 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Talhão");
+    var labelText = "";
+    size = 0;
+    var labelFont = "18.2px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.5000000000000004;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Talhão") !== null) {
+        labelText = String(feature.get("Talhão"));
+    }
+    
+var style = categories_TalhesProdutivos_13(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
